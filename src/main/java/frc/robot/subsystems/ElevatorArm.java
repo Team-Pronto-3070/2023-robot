f90ed71b930e07162e08b5fc4e74120488e3d939
kirill <kirillh@outlook.com>
2023-02-09 16:05:30 -0800

    added calculateFromPolar func

--- src/main/java/frc/robot/subsystems/ElevatorArm.java
+++ src/main/java/frc/robot/subsystems/ElevatorArm.java
@@ -1,10 +1,25 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Constants;
+
 // file
 // this is a cmomment
 // this is another cmomment
 //Hello world x10000
 
 public class ElevatorArm {
-    
+
+    Rotation2d target_angle = new Rotation2d();
+    double target_magnitude = 0.0;
+
+    /**
+     * the coordanates are absolute (not relative to arm base)
+     * @param cartesian
+     */
+    private void calculateFromPolar(Translation2d cartesian) {
+        target_angle = cartesian.getAngle();
+        target_magnitude = cartesian.getDistance(Constants.ElevatorArm.armOffset);
+    }
 }
