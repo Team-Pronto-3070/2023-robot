b89f3cbd72559bbbbedcb20c8bc2db4e43cc2312
unknown <julianrwalston@gmail.com>
2023-02-24 14:13:10 -0800

    added events to eventmap and added auto paths to autoChooser. Also updated buildAuto to load pathGroups instead of trajectories

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -9,6 +9,7 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -18,8 +19,27 @@ public class Autos {
     private final SendableChooser<Command> autoChooser;
 
     public Autos(SwerveSubsystem swerve) {
+
+        // TODO - map to commands
         HashMap<String, Command> eventMap = new HashMap<>();
-        eventMap.put("testprint", new PrintCommand("successfully ran a print command"));
+        eventMap.put("armToGroundIntake", new InstantCommand());
+        eventMap.put("armToShelfIntake", new InstantCommand());
+        eventMap.put("armToLevel1Cone", new InstantCommand());
+        eventMap.put("armToLevel1Cube", new InstantCommand());
+        eventMap.put("armToLevel2Cone", new InstantCommand());
+        eventMap.put("armToLevel2Cube", new InstantCommand());
+        eventMap.put("armToLevel3Cone", new InstantCommand());
+        eventMap.put("armToLevel3Cube", new InstantCommand());
+        eventMap.put("resetArm", new InstantCommand());
+        eventMap.put("scoreLevel1Cone", new InstantCommand());
+        eventMap.put("scoreLevel1Cube", new InstantCommand());
+        eventMap.put("scoreLevel2Cone", new InstantCommand());
+        eventMap.put("scoreLevel2Cube", new InstantCommand());
+        eventMap.put("scoreLevel3Cone", new InstantCommand());
+        eventMap.put("scoreLevel3Cube", new InstantCommand());
+        eventMap.put("intakeCone", new InstantCommand());
+        eventMap.put("intakeCube", new InstantCommand());
+        eventMap.put("autoBalance", new InstantCommand());
 
         autoBuilder = new SwerveAutoBuilder(
             swerve::getPose, // Pose2d supplier
@@ -35,11 +55,20 @@ public class Autos {
 
         autoChooser = new SendableChooser<Command>();
         autoChooser.setDefaultOption("none", new PrintCommand("no auto option selected"));
-        autoChooser.addOption("test1", buildAuto("test1"));
+        autoChooser.addOption("0 Piece Taxi Balance", buildAuto("0 Piece Taxi Balance"));
+        autoChooser.addOption("1 Piece Taxi LZ", buildAuto("1 Piece Taxi LZ"));
+        autoChooser.addOption("2 Piece Balance LZ", buildAuto("2 Piece Balance LZ"));
+        autoChooser.addOption("2 Piece No Balance LZ", buildAuto("2 Piece No Balance LZ"));
+        autoChooser.addOption("3 Piece No Balance LZ", buildAuto("3 Piece No Balance LZ"));
+        autoChooser.addOption("1 Piece Taxi WALL", buildAuto("1 Piece Taxi WALL"));
+        autoChooser.addOption("2 Piece Balance WALL", buildAuto("2 Piece Balance WALL"));
+        autoChooser.addOption("2 Piece No Balance WALL", buildAuto("2 Piece No Balance WALL"));
+        autoChooser.addOption("3 Piece No Balance WALL", buildAuto("3 Piece No Balance WALL"));
+
     }
 
     public Command buildAuto(String name) {
-        return autoBuilder.fullAuto(PathPlanner.loadPath(name, new PathConstraints(Constants.Auto.maxVelocity, Constants.Auto.maxAcceleration)));
+        return autoBuilder.fullAuto(PathPlanner.loadPathGroup(name, new PathConstraints(Constants.Auto.maxVelocity, Constants.Auto.maxAcceleration)));
     }
 
     public Command getSelectedAuto() {
