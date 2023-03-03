5e3e8ad205f312595a0455e8b4e1497c2b64d47e
unknown <julianrwalston@gmail.com>
2023-03-02 19:27:29 -0800

    added game object to intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,12 +7,15 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.GameObject;
 
 public class Intake extends SubsystemBase {
     
     private final WPI_TalonSRX talIntake;
 
     private final DigitalInput openedSwitch;
+
+    private GameObject gameObject = GameObject.NONE;
 
     public Intake() {
         talIntake = new WPI_TalonSRX(Constants.Intake.ID);
@@ -30,6 +33,14 @@ public class Intake extends SubsystemBase {
      */
     public void set(double velocity) {
         talIntake.set(velocity);
+    }
+
+    public GameObject getGameObject() {
+        return gameObject;
+    }
+
+    public void setGameObject(GameObject object) {
+        gameObject = object;
     }
 
     public Command closeCommand() {
