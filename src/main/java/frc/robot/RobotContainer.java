b2f9254877ab3bae79c6bcc3be5b74e087fe4422
unknown <julianrwalston@gmail.com>
2023-03-07 21:13:42 -0800

    hopefully fixed go to next arm position and go to next scoring slot functionality

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,6 +43,7 @@ public class RobotContainer {
     intake.setDefaultCommand(intake.run(intake::stop));
 
     nextArmPosition = Position.HOME;
+    nextScoringSlot = 0;
 
     configureBindings();
   }
@@ -57,17 +58,16 @@ public class RobotContainer {
 
     //TODO - check for cones vs cubes for arm positions
 
-    oi.armToNextTargetPositionButton.onTrue(elevatorArm.goToTargetCommand(nextArmPosition));
+    oi.armToNextTargetPositionButton.onTrue(elevatorArm.goToTargetCommand(() -> nextArmPosition));
     oi.armToShelfIntakePositionButton.onTrue(elevatorArm.goToTargetCommand(Position.SHELF));
     oi.armToGroundIntakePositionButton.onTrue(elevatorArm.goToTargetCommand(Position.L1CONE)); 
     oi.armToHomePositionButton.onTrue(elevatorArm.goToTargetCommand(Position.HOME));
 
     oi.fullAutoScore.onTrue(
-      new AutoScoringTrajectoryCommand(nextScoringSlot, new PathConstraints(4, 3), autos.autoBuilder, swerve)
-      .alongWith(elevatorArm.goToTargetCommand(nextArmPosition))
-      //.andThen(intake.openCommand())
+      new AutoScoringTrajectoryCommand(() -> nextScoringSlot, new PathConstraints(4, 3), autos.autoBuilder, swerve)
+      .alongWith(elevatorArm.goToTargetCommand(() -> nextArmPosition))
     );
-    oi.driveToScoringNodeButton.onTrue(new AutoScoringTrajectoryCommand(nextScoringSlot, new PathConstraints(4, 3), autos.autoBuilder, swerve)); //TODO determine path constraints
+    oi.driveToScoringNodeButton.onTrue(new AutoScoringTrajectoryCommand(() -> nextScoringSlot, new PathConstraints(4, 3), autos.autoBuilder, swerve)); //TODO determine path constraints
     oi.gyroResetButton.onTrue(swerve.runOnce(swerve::resetGyro));
     oi.interruptButton.onTrue(new InstantCommand(elevatorArm::stop, elevatorArm))
                       .onTrue(new InstantCommand(swerve::stop, swerve))
